930e34f8a8bb4adddc7c37d9a425820c3177ead9
Maksym Solomko <lena06051981be@gmail.com>
2022-09-30 19:17:46 +0300

    Update 01-color-switcher.js

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,19 @@
-console.log('rrr');
+const refs = {
+  startBtn: document.querySelector('button[data-start]'),
+  stopBtn: document.querySelector('button[data-stop]'),
+};
+console.log(refs.startBtn);
+console.log(refs.stopBtn);
+
+refs.startBtn.addEventListener('click', startChangeColorBody);
+
+function startChangeColorBody(evt) {
+  let color = getRandomHexColor();
+  //   let color = `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+  console.log(color);
+  document.body.style.backgroundColor = color;
+}
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
